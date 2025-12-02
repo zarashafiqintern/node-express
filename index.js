1f932db2c2e618336550394c94bb74b6a38f0417
Zara Shafiq <zarashafiq.intern@devsinc.com>
2025-12-02 22:07:16 +0500

    implemented notes routes

--- index.js
+++ index.js
@@ -1,56 +1,9 @@
 const express = require("express");
-const fs = require("fs");
-const notes = require("./MOCK_DATA.json");
 const app = express();
-
 app.use(express.json());
 
-app.get("/notes", (req, res) => {
-  return res.json(notes);
-});
-
-app.route("/notes/:id")
-  .get((req, res) => {
-    const id = parseInt(req.params.id);
-    const note = notes.find((note) => note.id === id);
-    return res.json(note);
-  })
-
-.put((req, res) => {
-    const id = parseInt(req.params.id);
-    const noteIndex = notes.findIndex((note) => note.id === id);
-    if (noteIndex === -1) {
-        return res.send("Note not found");
-    }
-    const updatedNote = req.body;   
-    notes[noteIndex] = { id, ...updatedNote };
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(notes), (err) => {
-        return res.json(notes[noteIndex]);
-    })
-})
-
-   .delete((req, res) => { 
-    const id = parseInt(req.params.id);
-    const noteIndex = notes.findIndex((note) => note.id === id);
-    if (noteIndex === -1) {
-        return res.send("id not found");
-    }
-    const deletedNote = notes.splice(noteIndex, 1);
-    fs.writeFile('./MOCK_DATA.json', JSON.stringify(notes), (err) => {
-        return res.send(JSON.stringify(notes))
-    });
-});
-
-app.post("/notes", (req, res) => {
-  const body = req.body;
-  notes.push({ id: notes.length + 1, ...body });
-  fs.writeFile("./MOCK_DATA.json", JSON.stringify(notes), (err) => {
-    return res.json({
-         status: "Success",
-          id: notes.length
-         });
-  });
-});
+const noteRoutes = require('./routes/notes')
+app.use('/', noteRoutes)
 
 app.listen(8000, () => {
   console.log("Server Started");
---
+++ routes/notes.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router =  express.Router();
+const fs = require("fs");
+const notes = require("../MOCK_DATA.json");
+
+router.get("/notes", (req, res) => {
+  return res.json(notes);
+});
+
+router.route("/notes/:id")
+  .get((req, res) => {
+    const id = parseInt(req.params.id);
+    const note = notes.find((note) => note.id === id);
+    return res.json(note);
+  })
+
+.put((req, res) => {
+    const id = parseInt(req.params.id);
+    const noteIndex = notes.findIndex((note) => note.id === id);
+    if (noteIndex === -1) {
+        return res.send("Note not found");
+    }
+    const updatedNote = req.body;   
+    notes[noteIndex] = { id, ...updatedNote };
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(notes), (err) => {
+        return res.json(notes[noteIndex]);
+    })
+})
+
+   .delete((req, res) => { 
+    const id = parseInt(req.params.id);
+    const noteIndex = notes.findIndex((note) => note.id === id);
+    if (noteIndex === -1) {
+        return res.send("id not found");
+    }
+    const deletedNote = notes.splice(noteIndex, 1);
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(notes), (err) => {
+        return res.send(JSON.stringify(notes))
+    });
+});
+
+router.post("/notes", (req, res) => {
+  const body = req.body;
+  notes.push({ id: notes.length + 1, ...body });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(notes), (err) => {
+    return res.json({
+         status: "Success",
+          id: notes.length
+         });
+  });
+});
+
+module.exports = router;
